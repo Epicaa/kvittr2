6091ec559455ed0c14be3827bfaedb1f1d540369
Erika <erika.sandbaek@hotmail.com>
2015-05-19 20:14:54 +0200

    added to models for pips

--- kvittr/pips/models.py
+++ kvittr/pips/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    study = models.CharField(max_length=255)
+    passed_exams = models.PositiveIntegerField(default=0)
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.name, self.email)
+
+class StudentNote(models.Model):
+    student = models.ForeignKey(Student, related_name='notes')
+    note = models.TextField()
+    points = models.PositiveIntegerField(default=0)
+    created_by = models.ForeignKey(User, related_name="created_notes")
+    created_datetime = models.DateTimeField()
+
+    def __unicode__(self):
+        return u'%s' % self.note
+
+    class Meta:
+        ordering = ['-id']
+
+
